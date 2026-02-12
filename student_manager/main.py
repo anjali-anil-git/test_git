a85eaf3921761f78e8a514cce1b7df954fc436e1
Ash <ashketch05@gmail.com>
2026-02-12 20:18:09 +0530

    Changed title in conflict-demo branch

---
+++ student_manager/main.py
@@ -0,0 +1,28 @@
+from manager import StudentManager
+
+manager = StudentManager()
+manager.load_from_file()
+
+while True:
+    print("\n--- STUDENT SYSTEM (Conflict Version A) ---")
+    print("1. Add Student")
+    print("2. View Students")
+    print("3. Save & Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        name = input("Enter student name: ")
+        roll_no = input("Enter roll number: ")
+        manager.add_student(name, roll_no)
+
+    elif choice == "2":
+        manager.show_students()
+
+    elif choice == "3":
+        manager.save_to_file()
+        print("Data saved. Exiting program.")
+        break
+
+    else:
+        print("Invalid choice. Try again.")
